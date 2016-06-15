2a414fdb883ec8784328fad0add06d95188293fe
Abed Halawi <halawi.abed@gmail.com>
2016-06-15 23:05:53 +0300

    add RespondWithJsonErrorJob

---
+++ src/Domains/Http/Jobs/RespondWithJsonErrorJob.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Trellis\Domains\Http\Jobs;
+
+use Illuminate\Routing\ResponseFactory;
+
+class RespondWithJsonErrorJob extends RespondWithJsonJob
+{
+    public function __construct($message = 'An error occurred', $code = 400, $status = 400, $headers = [], $options = 0)
+    {
+        $this->content = [
+            'status' => $status,
+            'error' => [
+                'code' => $code,
+                'message' => $message,
+            ],
+        ];
+
+        $this->status = $status;
+        $this->headers = $headers;
+        $this->options = $options;
+    }
+
+    public function handle(ResponseFactory $response)
+    {
+        return $response->json($this->content, $this->status, $this->headers, $this->options);
+    }
+}

738de3e4a689c0509ce2650710582204f93997f2
Adib Hanna <adibhanna@gmail.com>
2017-02-10 17:12:24 -0800

    add lucid services providers

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => 'Laravel',
+    'name' => 'My Application',
 
     /*
     |--------------------------------------------------------------------------
@@ -176,6 +176,9 @@ return [
         // Framework\Providers\BroadcastServiceProvider::class,
         Framework\Providers\EventServiceProvider::class,
         Framework\Providers\RouteServiceProvider::class,
+
+        App\Foundation\ServiceProvider::class,
+        Lucid\Console\LucidServiceProvider::class,
 
     ],
 

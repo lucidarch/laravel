f658d2287a14a01e4206f52dc7b6be8adeed41bd
Victorien Plancke <fb@vito.fr.nf>
2017-07-21 15:54:40 +0200

    Fix User class namespace

--- config/auth.php
+++ config/auth.php
@@ -67,7 +67,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\User::class,
+            'model' => Framework\User::class,
         ],
 
         // 'users' => [
